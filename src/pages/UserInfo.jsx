eca0ad6a686613c042483f325ad23837c52a7aab
9898632403 <meetmochi2403@gmail.com>
2025-07-14 18:22:15 +0530

    chore: update latest changes 🚀

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import Select from 'react-select';
-
+import AsyncSelect from 'react-select/async';
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
